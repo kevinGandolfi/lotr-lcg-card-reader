19917bec502195db8f46237d792d95d43c40a123
Kevin GANDOLFI <gandolfi.kevin@gmail.com>
2024-03-02 19:25:54 +0100

    feat(card): implement get bottom label method

--- CardReaders/CardReader.cs
+++ CardReaders/CardReader.cs
@@ -12,35 +12,6 @@ namespace CardReaders
 
     #region PUBLIC METHODS
 
-    public async Task<MemoryStream> GetMemoryStreamFromHostedImage(string imageUrl)
-    {
-      using (var httpClient = new HttpClient())
-      {
-        try
-        {
-          var response = await httpClient.GetByteArrayAsync(imageUrl);
-          return new MemoryStream(response);
-        }
-        catch (Exception)
-        {
-          throw new InvalidOperationException("The image could not be found");
-        }
-      }
-    }
-
-    /// <summary>
-    /// Loads an image and stores it into the Image property.
-    /// </summary>
-    /// <param name="sourcePath">Source path of the image.</param>
-    public void Load(MemoryStream imageStream)
-    {
-      this.Image = new MagickImage(imageStream)
-      {
-        HasAlpha = true
-      };
-    }
-
-
     #endregion PUBLIC METHODS
   }
 }
