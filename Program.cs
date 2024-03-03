6aef0777997a31f24e08d9c635949c01d4d13fc2
Kevin GANDOLFI <kevin.gandolfi@alten.com>
2024-03-03 12:47:32 +0100

    feat(card): implement get type (wip) using tesseract

--- Program.cs
+++ Program.cs
@@ -1,17 +1,15 @@
-﻿using System.IO;
-using System.Runtime.CompilerServices;
-using ImageMagick;
+﻿using ImageMagick;
 using LOTR_CR.CardReaders.Models;
-using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
   static void Main(string[] args)
   {
     Console.WriteLine("Welcome to the card reader of Lord of the Rings: the Card Game");
-    Card card = new("https://sda-src.cgbuilder.fr/images/carte/16/2.jpg");
+    Card card = new("https://sda-src.cgbuilder.fr/images/carte/16/18.jpg");
     MagickImage bottomLabel = card.GetBottomLabel();
     bottomLabel.Format = MagickFormat.Jpg;
     bottomLabel.Write(@$"D:\\bottom_label.jpg");
+    card.GetCardType();
   }
 }
