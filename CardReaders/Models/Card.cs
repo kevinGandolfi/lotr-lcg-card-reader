25f272d77a5c53c5fcd469bb42c83b97e7c3b021
Kevin GANDOLFI <kevin.gandolfi@alten.com>
2024-03-10 12:04:27 +0100

    feat(card): refacto code

--- CardReaders/Models/Card.cs
+++ CardReaders/Models/Card.cs
@@ -3,16 +3,39 @@ using Tesseract;
 
 namespace LOTR_CR.CardReaders.Models
 {
+  /// <summary>
+  /// Class representing a LOTR:TCG card.
+  /// </summary>
   public class Card
   {
-    private MagickImage _bottom_label = new();
+    #region CONSTANTS
+
+    private const string _COLOR_SPECIFIC_TO_PLAYER_CARDS = "#E2CFC8";
+    private const string _BOTTOM_LABEL_FILE_NAME = @"..\..\..\bottom_label.jpg";
+
+    #endregion CONSTANTS
+
+    #region FIELDS
+
+    private MagickImage _bottom_label;
+
+    #endregion FIELDS
 
     #region PROPERTIES
 
+    /// <summary>
+    /// Type of the card.
+    /// </summary>
     public CardType Type { get; set; }
 
+    /// <summary>
+    /// Name of the card. (Optional)
+    /// </summary>
     public string Name { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Image of the card.
+    /// </summary>
     public MagickImage CardImage { get; set; } = new();
 
     #endregion PROPERTIES
@@ -21,9 +44,11 @@ namespace LOTR_CR.CardReaders.Models
     {
       MemoryStream stream = this.GetMemoryStreamFromHostedImage(imageUrl);
       this.Load(stream);
+      this._bottom_label = (MagickImage)this.CardImage.Clone();
+      this.GetCardType();
     }
 
-    #region PUBLIC METHODS
+    #region PRIVATE METHODS
 
     /// <summary>
     /// Gets a stream of a picture hosted on the internet.
@@ -31,7 +56,7 @@ namespace LOTR_CR.CardReaders.Models
     /// <param name="imageUrl">URL of the picture.</param>
     /// <returns></returns>
     /// <exception cref="InvalidOperationException"></exception>
-    public MemoryStream GetMemoryStreamFromHostedImage(string imageUrl)
+    private MemoryStream GetMemoryStreamFromHostedImage(string imageUrl)
     {
       using (var httpClient = new HttpClient())
       {
@@ -51,7 +76,7 @@ namespace LOTR_CR.CardReaders.Models
     /// Loads an image and stores it into the Image property.
     /// </summary>
     /// <param name="sourcePath">Source path of the image.</param>
-    public void Load(MemoryStream imageStream)
+    private void Load(MemoryStream imageStream)
     {
       this.CardImage = new MagickImage(imageStream)
       {
@@ -63,38 +88,110 @@ namespace LOTR_CR.CardReaders.Models
     /// Gets the card type of the current card.
     /// </summary>
     /// <exception cref="NotImplementedException"></exception>
-    public void GetCardType()
+    private void GetCardType()
     {
-      this.GetBottomLabel();
-      //string? bottomLabelFileName = "./bottom_label.jpg";
-      this._bottom_label.Write(@"D:\bottom_label.jpg");
-      //this._bottom_label.Write(bottomLabelFileName);
-
-      using (var engine = new TesseractEngine("./tessdata", "fra", EngineMode.Default))
+      if (this.CardImage.Width == 560 && this.CardImage.Height == 394)
       {
-        using (var img = Pix.LoadFromFile(@"D:\bottom_label.jpg"))
-        {
-          using (var page = engine.Process(img))
-          {
-            var text = page.GetText();
-          }
-        }
+        this.Type = CardType.Quest;
+        return;
       }
+
+      //if (this.IsEncounterCard())
+      //{
+      //  this.GetBottomLabelOfEncounterCard();
+      //}
+      //else
+      //{
+      //  this.GetBottomLabelOfPlayerCard();
+      //}
+      this.GetBottomLabel();
+      string labelText = this.GetLabelText();
+
     }
 
     /// <summary>
     /// Gets the bottom part of a card in order to later read it.
     /// </summary>
     /// <exception cref="NotImplementedException"></exception>
-    private void GetBottomLabel()
+    private void GetBottomLabelOfEncounterCard()
     {
-      MagickImage image = this.CardImage;
-      image.Crop(0, 43, Gravity.South);
-      image.Extent(image.Width, image.Height - 29, Gravity.North);
-      image.Extent(140, image.Height, Gravity.Center);
-      this._bottom_label = image;
+      this._bottom_label.Crop(0, 38, Gravity.South);
+      this._bottom_label.Extent(this._bottom_label.Width, this._bottom_label.Height - 20, Gravity.North);
+      this._bottom_label.Extent(140, this._bottom_label.Height, Gravity.Center);
+      this._bottom_label.Write(_BOTTOM_LABEL_FILE_NAME);
     }
 
-    #endregion PUBLIC METHODS
+    /// <summary>
+    /// Gets the bottom part of a card in order to later read it.
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
+    private void GetBottomLabelOfPlayerCard()
+    {
+      this._bottom_label.Crop(0, 38, Gravity.South);
+      this._bottom_label.Extent(this._bottom_label.Width, this._bottom_label.Height - 20, Gravity.North);
+      this._bottom_label.Extent(140, this._bottom_label.Height, Gravity.Center);
+      this._bottom_label.Write(_BOTTOM_LABEL_FILE_NAME);
+    }
+
+    private void GetBottomLabel()
+    {
+      this._bottom_label.Crop(0, 38, Gravity.South);
+      this._bottom_label.Extent(this._bottom_label.Width, this._bottom_label.Height - 20, Gravity.North);
+      this._bottom_label.Extent(140, this._bottom_label.Height, Gravity.Center);
+      this._bottom_label.BackgroundColor = MagickColors.White;
+      this._bottom_label.Write(_BOTTOM_LABEL_FILE_NAME);
+    }
+
+    /// <summary>
+    /// Checks if a color exists in a zone where it may be found in a player card.
+    /// </summary>
+    /// <returns>False if the color exists, true otherwise.</returns>
+    private bool IsEncounterCard()
+    {
+      int startX = 101; // X-coordinate of the starting point of the zone
+      int startY = 526; // Y-coordinate of the starting point of the zone
+      int width = 10; // Width of the zone
+      int height = 10; // Height of the zone
+
+      MagickGeometry geometry = new MagickGeometry(startX, startY, width, height);
+      IMagickImage<ushort> image = this.CardImage.Clone();
+      image.Crop(geometry);
+      MagickColor targetColor = new(_COLOR_SPECIFIC_TO_PLAYER_CARDS);
+
+      using (IPixelCollection<ushort> pixels = image.GetPixels())
+      {
+        foreach (IPixel<ushort> pixel in pixels)
+        {
+          if ((MagickColor)pixel.ToColor()! == targetColor)
+          {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+
+    private string GetLabelText()
+    {
+      string labelText = string.Empty;
+
+      using (var engine = new TesseractEngine(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\tessdata")), "fra", EngineMode.TesseractAndLstm))
+      {
+        using (var img = Pix.LoadFromFile(_BOTTOM_LABEL_FILE_NAME))
+        {
+          var path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\tessdata"));
+          Console.WriteLine($"tessdata: {path}");
+          using (var page = engine.Process(img, PageSegMode.SingleWord))
+          {
+            labelText = page.GetText();
+          }
+        }
+      }
+
+      return labelText;
+    }
+
+    #endregion PRIVATE METHODS
   }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ class Program
   static void Main(string[] args)
   {
     Console.WriteLine("Welcome to the card reader of Lord of the Rings: the Card Game");
-    Card card = new("https://sda-src.cgbuilder.fr/images/carte/16/28.jpg");
-    card.GetCardType();
+    Card card = new("https://sda-src.cgbuilder.fr/images/carte/16/10.jpg");//10 is player, 23 is encounter (debug)
   }
 }
