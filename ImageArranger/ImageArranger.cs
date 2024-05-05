8ac25ad4b905d22699fdf80bc78fb14bee854d1a
Kevin GANDOLFI <gandolfi.kevin@gmail.com>
2024-05-05 12:38:01 +0200

    feat(image-arranger): add image arranger project and page to print model (wip)

---
+++ ImageArranger/ImageArranger.cs
@@ -0,0 +1,33 @@
+﻿using ImageArranger.Models;
+using ImageMagick;
+
+namespace ImageArranger;
+
+public class ImageArranger
+{
+  public const int MARGIN = 10;
+
+  private int _maxWidth = 0;
+  private int _maxHeight = 0;
+
+  public IEnumerable<MagickImage> imagesToPrint = [];
+  public List<PageToPrint> PagesToPrint { get; set; } = [];
+
+  public ImageArranger()
+  {
+    this._maxHeight = imagesToPrint.Max(i => i.Height);
+    this._maxWidth = imagesToPrint.Max(i => i.Width);
+  }
+
+  /// <summary>
+  /// Arranges all images onto A4 pages.
+  /// </summary>
+  public void ArrangeOnPage()
+  {
+    foreach (MagickImage imageToPrint in imagesToPrint)
+    {
+      this.PagesToPrint.Add(new PageToPrint(this._maxWidth, this._maxHeight, MARGIN));
+      //pageImage.Composite(imageToPrint, lastXPosition, lastYPosition, CompositeOperator.Over);
+    }
+  }
+}
---
+++ ImageArranger/Models/PageToPrint.cs
@@ -0,0 +1,57 @@
+﻿using ImageMagick;
+
+namespace ImageArranger.Models
+{
+  /// <summary>
+  /// Represents the page to print with images.
+  /// </summary>
+  public class PageToPrint
+  {
+    /// <summary>
+    /// Page to print.
+    /// </summary>
+    public MagickImage PageImage = new (MagickColors.White, 2480, 3508);
+
+    /// <summary>
+    /// List of positions for the images. The boolean value is set to false if the position is not taken, true otherwise.
+    /// </summary>
+    public Dictionary<(int x, int y), bool> Positions { get; set; } = [];
+
+    public bool IsFull { get; set; } = false;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="imageToPrintWidth">Max width of the images to print.</param>
+    /// <param name="imageToPrintHeight">Max height of the images to print.</param>
+    /// <param name="margin">Margin between the images.</param>
+    public PageToPrint(int imageToPrintWidth, int imageToPrintHeight, int margin)
+    {
+      int lastXPosition = margin;
+      int lastYPosition = margin;
+      while (this.HasEnoughHeight(imageToPrintHeight, lastYPosition, margin))
+      {
+        Positions.Add((lastXPosition, lastYPosition), false);
+        if (this.HasEnoughWidth(imageToPrintWidth, lastXPosition, margin))
+        {
+          lastXPosition += imageToPrintWidth + margin;
+        }
+        else
+        {
+          lastXPosition = margin;
+          lastYPosition += imageToPrintHeight + margin;
+        }
+      }
+    }
+
+    private bool HasEnoughWidth(int imageToPrintWidth, int lastXPosition, int margin)
+    {
+      return lastXPosition - this.PageImage.Width > imageToPrintWidth + margin;
+    }
+
+    private bool HasEnoughHeight(int imageToPrintHeight, int lastYPosition, int margin)
+    {
+      return lastYPosition - this.PageImage.Width > imageToPrintHeight + margin;
+    }
+  }
+}
