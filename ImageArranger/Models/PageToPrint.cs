107bc487f048eb98d5cf5d15fda3f2b4f6525880
Kevin GANDOLFI <gandolfi.kevin@gmail.com>
2024-05-06 17:40:32 +0200

    feat(page-arranger): work on page arranger (not tested)

--- ImageArranger/Models/PageToPrint.cs
+++ ImageArranger/Models/PageToPrint.cs
@@ -7,15 +7,23 @@ namespace ImageArranger.Models
   /// </summary>
   public class PageToPrint
   {
+    private const string FILE_EXTENSION_OUTPUT = ".png";
+
+    private const string _directoryPath = @"..\..\..\PagesToPrint\";
+
+    #region PROPERTIES
+
     /// <summary>
     /// Page to print.
     /// </summary>
-    public MagickImage PageImage = new (MagickColors.White, 2480, 3508);
+    public MagickImage PageImage { get; init; }
 
     /// <summary>
     /// List of positions for the images. The boolean value is set to true if the position is available, false otherwise.
     /// </summary>
     public Dictionary<(int x, int y), bool> Positions { get; set; } = [];
+
+    #endregion PROPERTIES
 
     /// <summary>
     /// Constructor.
@@ -27,6 +35,7 @@ namespace ImageArranger.Models
     {
       int lastXPosition = margin;
       int lastYPosition = margin;
+      this.PageImage = new(MagickColors.White, 2480, 3508);
       while (this.HasEnoughHeight(imageToPrintHeight, lastYPosition, margin))
       {
         Positions.Add((lastXPosition, lastYPosition), false);
@@ -42,6 +51,41 @@ namespace ImageArranger.Models
       }
     }
 
+    #region PUBLIC METHODS
+
+    /// <summary>
+    /// Saves the page image.
+    /// </summary>
+    /// <param name="pageNumber">Number of the page.</param>
+    public void SavePageImage(int pageNumber)
+    {
+      this.PageImage.Write($"{_directoryPath}{pageNumber}{FILE_EXTENSION_OUTPUT}");
+    }
+
+    /// <summary>
+    /// Checks if a page has available space to an image.
+    /// </summary>
+    /// <returns></returns>
+    public bool CanAddToPage()
+    {
+      return this.Positions.Any(p => p.Value);
+    }
+
+    /// <summary>
+    /// Adds an image to a page.
+    /// </summary>
+    /// <param name="imageToPrint"></param>
+    public void AddToPage(MagickImage imageToPrint)
+    {
+      (int x, int y) = this.Positions.First(p => p.Value).Key;
+      this.PageImage.Composite(imageToPrint, x, y, CompositeOperator.Over);
+      this.Positions[(x, y)] = false;
+    }
+
+    #endregion PUBLIC METHODS
+
+    #region PRIVATE METHODS
+
     private bool HasEnoughWidth(int imageToPrintWidth, int lastXPosition, int margin)
     {
       return lastXPosition - this.PageImage.Width > imageToPrintWidth + margin;
@@ -51,5 +95,7 @@ namespace ImageArranger.Models
     {
       return lastYPosition - this.PageImage.Width > imageToPrintHeight + margin;
     }
+
+    #endregion PRIVATE METHODS
   }
 }
---
+++ ImageArranger/PageArranger.cs
@@ -0,0 +1,66 @@
+﻿using ImageArranger.Models;
+using ImageMagick;
+
+namespace ImageArranger;
+
+/// <summary>
+/// Arranges images to print on a page.
+/// </summary>
+public class PageArranger
+{
+  #region CONSTANTS
+
+  private const int MARGIN = 10;
+
+  #endregion CONSTANTS
+
+  #region FIELDS
+
+  private readonly int _maxWidth;
+  private readonly int _maxHeight;
+  private readonly IEnumerable<MagickImage> _imagesToPrint = [];
+
+  #endregion FIELDS
+
+  #region CONSTRUCTORS
+
+  /// <summary>
+  /// Constructor.
+  /// </summary>
+  public PageArranger(IEnumerable<MagickImage> imagesToPrint)
+  {
+    this._imagesToPrint = imagesToPrint;
+    this._maxHeight = _imagesToPrint.Max(i => i.Height);
+    this._maxWidth = _imagesToPrint.Max(i => i.Width);
+  }
+
+  #endregion CONSTRUCTORS
+
+  #region PUBLIC METHODS
+
+  /// <summary>
+  /// Arranges all images onto (A4) pages.
+  /// </summary>
+  public void ArrangeOnPage()
+  {
+    PageToPrint pageToPrint = new(this._maxWidth, this._maxHeight, MARGIN);
+    int pageNumber = 1;
+    foreach (MagickImage imageToPrint in _imagesToPrint)
+    {
+      if (pageToPrint.CanAddToPage())
+      {
+        pageToPrint.AddToPage(imageToPrint);
+      }
+      else
+      {
+        pageToPrint.SavePageImage(pageNumber);
+        pageNumber++;
+        pageToPrint.PageImage.Dispose();
+        pageToPrint = new(this._maxWidth, this._maxHeight, MARGIN);
+        pageToPrint.AddToPage(imageToPrint);
+      }
+    }
+  }
+
+  #endregion PUBLIC METHODS
+}
